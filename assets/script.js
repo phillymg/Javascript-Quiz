3eb02e6b54b1407155a07cabe8aa9d49455a2c12
Philmg <phillipmguetterman@att.net>
2023-07-13 22:12:11 -0500

    Restart

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,89 @@
-const questions = [
-    {
-        q: "What is 2 + 2?"
-        a: [
-            {text: "6", isCorrect: false},
-            {text: "3", isCorrect: false},
-            {text: "22", isCorrect: false},
-            {text: "4", isCorrect: true}
-        ]
+// Questions that will be asked
+const Questions = [{
+    q: "What is capital of India?",
+    a: [{ text: "Gandhinagar", isCorrect: false },
+    { text: "Surat", isCorrect: false },
+    { text: "Delhi", isCorrect: true },
+    { text: "Mumbai", isCorrect: false }
+    ]
 
-    },
-    {
-        q: "What is 4 + 2?"
-        a: [
-            {text: "6", isCorrect: true},
-            {text: "3", isCorrect: false},
-            {text: "42", isCorrect: false},
-            {text: "4", isCorrect: false}
-        ]
+},
+{
+    q: "What is the capital of Thailand?",
+    a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+    { text: "Phuket", isCorrect: false },
+    { text: "Ayutthaya", isCorrect: false },
+    { text: "Bangkok", isCorrect: true }
+    ]
 
-    },
-    {
-        q: "What is 2 - 1?"
-        a: [
-            {text: "6", isCorrect: false},
-            {text: "1", isCorrect: true},
-            {text: "21", isCorrect: false},
-            {text: "4", isCorrect: false}
-        ]
+},
+{
+    q: "What is the capital of Gujarat",
+    a: [{ text: "Surat", isCorrect: false },
+    { text: "Vadodara", isCorrect: false },
+    { text: "Gandhinagar", isCorrect: true },
+    { text: "Rajkot", isCorrect: false }
+    ]
 
-    }
+}
+
 ]
 
-let currentQuestion = 0
+let currQuestion = 0
 let score = 0
 
-function loadQues () {
+function loadQues() {
     const question = document.getElementById("ques")
     const opt = document.getElementById("opt")
 
-    
+    question.textContent = Questions[currQuestion].q;
+    opt.innerHTML = ""
+
+    for (let i = 0; i < Questions[currQuestion].a.length; i++) {
+        const choicesdiv = document.createElement("div");
+        const choice = document.createElement("input");
+        const choiceLabel = document.createElement("label");
+
+        choice.type = "radio";
+        choice.name = "answer";
+        choice.value = i;
+
+        choiceLabel.textContent = Questions[currQuestion].a[i].text;
+
+        choicesdiv.appendChild(choice);
+        choicesdiv.appendChild(choiceLabel);
+        opt.appendChild(choicesdiv);
+    }
+}
+
+loadQues();
+
+function loadScore() {
+    const totalScore = document.getElementById("score")
+    totalScore.textContent = `You scored ${score} out of ${Questions.length}`
+}
+
+
+function nextQuestion() {
+    if (currQuestion < Questions.length - 1) {
+        currQuestion++;
+        loadQues();
+    } else {
+        document.getElementById("opt").remove()
+        document.getElementById("ques").remove()
+        document.getElementById("btn").remove()
+        loadScore();
+    }
+}
+
+function checkAns() {
+    const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
+
+    if (Questions[currQuestion].a[selectedAns].isCorrect) {
+        score++;
+        console.log("Correct")
+        nextQuestion();
+    } else {
+        nextQuestion();
+    }
 }
